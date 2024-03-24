27c83394ed2c84a5fddfa4d0d20bb63ee17709dd
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-03-24 23:15:47 +0600

    Added frontend domain in the backend as cors issues

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,14 @@ import userRouter from "./route/user.route.js";
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000/", "http://localhost:3000"], // Adjust if necessary
+    origin: [
+      "http://localhost:3000/",
+      "http://localhost:3000",
+      "https://rakib-portfolio-website.vercel.app/",
+      "https://www.rakib-portfolio-website.vercel.app/",
+      "http://rakib-portfolio-website.vercel.app/",
+      "http://www.rakib-portfolio-website.vercel.app/",
+    ], // Adjust if necessary
     credentials: true, // Enable cookies for cross-origin requests (if applicable)
   })
 );
