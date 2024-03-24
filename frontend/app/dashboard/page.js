d62cb439d21f09d7a513c43208c2dbf4566c6fa9
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-03-24 23:07:01 +0600

    Fixed possible error

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -13,7 +13,7 @@ async function getData() {
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+    return [];
   }
   return await res.json();
 }
