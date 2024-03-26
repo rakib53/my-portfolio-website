383cf26676890e51efb0cd44a6496b9d39ba5edc
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-03-26 17:11:02 +0600

    removed domain attributes from the cookie

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -19,8 +19,6 @@ const Login = async (req, res) => {
           expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
           httpOnly: true,
           secure: true,
-          sameSite: "none",
-          domain: ".vercel.app",
         })
         .status(200)
         .json({
